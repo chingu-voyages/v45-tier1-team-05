93239afbb177f836eb8a9f7434c56f3f3dd35f0f
LC <140190642+chef-louis@users.noreply.github.com>
2023-08-21 15:21:52 -0600

    Add map visualizations (#20)
    * feat: add leaflet library import
    
    * feat: add map container into html and read in map javascript code
    
    * feat: add assets data file and read into html
    
    * feat: add osm basemap tiles; draw markers onto canvas for improved performance; bind pop-ups onto markers

---
+++ js/map.js
@@ -0,0 +1,55 @@
+// Allow scroll
+const corner1 = L.latLng(-90, -200)
+const corner2 = L.latLng(90, 200)
+const bounds = L.latLngBounds(corner1, corner2)
+
+// Set use-canvas, zoom and map center options
+const map = new L.map('map', {
+    preferCanvas: true,
+    center: [39.75, -104.98],
+    zoom: 7,
+    minZoom: 3,
+    maxZoom: 16,
+    maxBounds: bounds,
+    maxBoundsViscosity: 0.5
+});
+
+// Add OSM basemap tiles
+let layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 20,
+	attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+});
+layer.addTo(map);
+
+var myRenderer = L.canvas({ padding: 0.5 });
+
+// Draw markers onto canvas and style with red
+const markerOptions = {
+    renderer: myRenderer,
+    radius: 7,
+    fillColor:"#ff0000",
+    color: "#ff0000",
+};
+
+// Filter out data points without valid lat/lng coordinates
+function coordinateFilter(feature) {
+    if (Array.isArray(feature.geometry.coordinates)) return true;
+}
+
+// Draw data points onto canvas tiles and bind pop-up info
+let dataLayer = L.geoJson(meteoriteData, {
+    filter: coordinateFilter,
+    onEachFeature: function (feature, layer) {
+        layer.bindPopup(`
+            <ul id="info" style="list-style: none;>
+                <li id="info-row"><strong>Name:</strong> ${feature.properties.name}</li>
+                <li id="info-row"><strong>Class:</strong> ${feature.properties.class}</li>
+                <li id="info-row"><strong>Mass (g):</strong> ${feature.properties["mass (g)"]}</li>
+                <li id="info-row"><strong>Year:</strong> ${feature.properties.year}</li>
+            </ul>
+        `);
+    },
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, markerOptions);
+    }
+}).addTo(map);
